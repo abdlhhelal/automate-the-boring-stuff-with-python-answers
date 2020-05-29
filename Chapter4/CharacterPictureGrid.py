5136e2662550ae5320360664957b5d600d3393ef
abdlhhelal <abdlhhelal@gmail.com>
2020-05-29 11:07:48 +0200

    Uploading CharacterPictureGrid

---
+++ Chapter4/CharacterPictureGrid.py
@@ -0,0 +1,18 @@
+def CharacterPictureGrid():
+    grid = [['.', '.', '.', '.', '.', '.'],
+            ['.', 'O', 'O', '.', '.', '.'],
+            ['O', 'O', 'O', 'O', '.', '.'],
+            ['O', 'O', 'O', 'O', 'O', '.'],
+            ['.', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', '.'],
+            ['O', 'O', 'O', 'O', '.', '.'],
+            ['.', 'O', 'O', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.']]
+            
+    for i in range (len(grid[0])):
+        for j in range ((len(grid)-1),-1,-1):
+            print(grid[j][i] , end='')
+        print()
+    return None
+
+CharacterPictureGrid()
