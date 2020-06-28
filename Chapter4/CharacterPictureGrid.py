5fdff07f9de1c2a5705c3cc5d62ce50e6bed916c
abdlhhelal <abdlhhelal@gmail.com>
2020-06-28 15:25:22 +0200

    testing git on a new windows installed

--- Chapter4/CharacterPictureGrid.py
+++ Chapter4/CharacterPictureGrid.py
@@ -8,7 +8,7 @@ def CharacterPictureGrid():
             ['O', 'O', 'O', 'O', '.', '.'],
             ['.', 'O', 'O', '.', '.', '.'],
             ['.', '.', '.', '.', '.', '.']]
-            
+
     for i in range (len(grid[0])):
         for j in range ((len(grid)-1),-1,-1):
             print(grid[j][i] , end='')
